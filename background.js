2dcc0418ef35669ccde30bbfaf692f22a30808b6
01chaitanya01 <choudharichaitu0101@gmail.com>
2025-02-22 21:22:36 +0530

    video added

--- background.js
+++ background.js
@@ -1,19 +1,55 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.declarativeNetRequest.updateDynamicRules({
-        addRules: [
-            {
-                id: 1,
-                priority: 1,
-                action: {
-                    type: "redirect",
-                    redirect: { extensionPath: "/redirect.html" }
-                },
-                condition: {
-                    urlFilter: "||youtube.com/*",
-                    resourceTypes: ["main_frame"]
-                }
-            }
-        ],
-        removeRuleIds: [1]
-    });
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+      {
+        id: 2,
+        priority: 1,
+        action: {
+          type: "redirect",
+          redirect: { extensionPath: "/redirect.html" },
+        },
+        condition: {
+          urlFilter: "*://*.facebook.com/*",
+          resourceTypes: ["main_frame"],
+        },
+      },
+      {
+        id: 3,
+        priority: 1,
+        action: {
+          type: "redirect",
+          redirect: { extensionPath: "/redirect.html" },
+        },
+        condition: {
+          urlFilter: "*://*.instagram.com/*",
+          resourceTypes: ["main_frame"],
+        },
+      },
+      {
+        id: 4,
+        priority: 1,
+        action: {
+          type: "redirect",
+          redirect: { extensionPath: "/redirect.html" },
+        },
+        condition: {
+          urlFilter: "*://*.twitter.com/*",
+          resourceTypes: ["main_frame"],
+        },
+      },
+      {
+        id: 5,
+        priority: 1,
+        action: {
+          type: "redirect",
+          redirect: { extensionPath: "/redirect.html" },
+        },
+        condition: {
+          urlFilter: "*://*.brave.com/*",
+          resourceTypes: ["main_frame"],
+        },
+      },
+    ],
+    removeRuleIds: [2, 3, 4, 5],
+  });
 });
